013350b52573d08649afc3b3d4696332a7383b48
Gabriel Echave <112594225+GabrielEchave@users.noreply.github.com>
2025-06-06 21:58:52 -0300

    Update conexion.java

--- src/clases/conexion.java
+++ src/clases/conexion.java
@@ -1,1 +1,15 @@
+package clases;
+import java.sql.*;
 
+
+public class Conexion {
+    public static Connection conectar(){
+        try {
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_ds", "root", "ADMIN");
+            return cn;
+        } catch (SQLException e) {
+            System.out.println("Error en la conexion local"+e);
+        }
+        return (null);
+    }
+}
